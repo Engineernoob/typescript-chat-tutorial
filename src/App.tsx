a688530d178fd63f84cfd476e5b94f57f0300f67
Engineernoob <126827805+Engineernoob@users.noreply.github.com>
2023-04-10 13:14:42 -0400

    adding information

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { MultiChatWindow } from "react-chat-engine-advanced";
+import { MultiChatWindow,useMultiChatLogic } from "react-chat-engine-advanced";
+
+const projectId: string = 'e7143ea6-51b3-49ad-b973-e78dc6f89ebd'
+const userName: string = 't.denmark@outlook.com'
+const secret: string = 'PsEttb6Y5Yjq9F5'
 
 function App() {
+    const chatProps = useMultiChatLogic(projectId, userName, secret)
     return <div>
-        <MultiChatWindow style={{ height:'100vh'}} />  
+        <MultiChatWindow {...chatProps} style={{ height:'100vh'}} />  
         </div>
 }
 

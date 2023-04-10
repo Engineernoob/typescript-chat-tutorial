380bbd7b37ff6ec879c07db719083b79a18a949f
Engineernoob <126827805+Engineernoob@users.noreply.github.com>
2023-04-10 13:20:02 -0400

    add message compents

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MultiChatWindow,useMultiChatLogic } from "react-chat-engine-advanced";
+import { MultiChatWindow,useMultiChatLogic, MultiChatSocket } from "react-chat-engine-advanced";
 
 const projectId: string = 'e7143ea6-51b3-49ad-b973-e78dc6f89ebd'
 const userName: string = 't.denmark@outlook.com'
@@ -7,6 +7,7 @@ const secret: string = 'PsEttb6Y5Yjq9F5'
 function App() {
     const chatProps = useMultiChatLogic(projectId, userName, secret)
     return <div>
+        <MultiChatSocket {...chatProps} />
         <MultiChatWindow {...chatProps} style={{ height:'100vh'}} />  
         </div>
 }

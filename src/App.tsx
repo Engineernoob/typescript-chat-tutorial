a3d1f0f426e8995ca12b962f95610f73ec450658
Engineernoob <126827805+Engineernoob@users.noreply.github.com>
2023-04-10 12:51:49 -0400

    add height and style

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { MultiChatWindow } from "react-chat-engine-advanced";
+
+function App() {
+    return <div>
+        <MultiChatWindow style={{ height:'100vh'}} />  
+        </div>
+}
+
+export default App;
